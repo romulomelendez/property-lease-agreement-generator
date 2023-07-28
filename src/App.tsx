d2a0965b08c3bc4985c2476308665dfad898ab80
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-07-28 20:02:44 -0300

    Feat: chakra ui lib instaled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
 import { Home } from './pages/Home'
 
 import { GlobalStyle } from './GlobalStyle'
 
 export const App: React.FC = () => (
-
-  <>
+  <ChakraProvider>
     <GlobalStyle />
     <Home />
-  </>
+  </ChakraProvider>
 )
