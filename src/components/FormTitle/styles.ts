65fc3ec41bb953df7f880cd5413eab5bc6694808
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-08-09 03:05:27 -0300

    initial commit

--- src/components/FormTitle/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const Title = styled.h5 `
-
-    font-size: 15pt;
-    font-weight: 700;
-    font-family: 'REM', sans-serif;
-
-`
--- src/components/PdfViewer/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const PdfContainer = styled.section `
-
-    height: 100vh;
-
-`
