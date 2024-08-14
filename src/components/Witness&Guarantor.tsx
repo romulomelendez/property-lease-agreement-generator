c829f36ed4f792b7f4012373ddbdd0d242cef172
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-08-14 11:24:43 -0300

    form title added in witness and guarantor form

--- src/components/Witness&Guarantor.tsx
+++ src/components/Witness&Guarantor.tsx
@@ -8,18 +8,23 @@ export const WitnessAndGuarantor: React.FC = () => {
     const [wantGuarantor, setWantGuarantor] = useState<boolean>(false)
 
     return (
-        <div className="flex flex-col gap-4">
-            {
-                wantWitness === false ?
-                    <button type="button" className="bg-blue-800 hover:bg-blue-700 text-white font-bold font-sans p-4 text-lg md:text-xl" onClick={() => setWantWitness(true)}>Add Witness(Optional)</button>
-                : <Witness />
-            }
+        <div className="flex flex-col gap-8">
+            <span className="font-bold font-sans text-center text-xl lg:text-3xl">
+                WITNESS & GUARANTOR(OPTIONAL)
+            </span>
+            <div className="flex flex-col gap-4">
+                {
+                    wantWitness === false ?
+                        <button type="button" className="bg-blue-800 hover:bg-blue-700 text-white font-bold font-sans p-4 text-lg md:text-xl" onClick={() => setWantWitness(true)}>Add Witness</button>
+                    : <Witness />
+                }
 
-            {
-                wantGuarantor === false ?
-                    <button type="button" className="bg-blue-800 hover:bg-blue-700 text-white font-bold font-sans p-4 text-lg md:text-xl" onClick={() => setWantGuarantor(true)}>Add Guarantor(Optional)</button>
-                : <Guarantor />
-            }
+                {
+                    wantGuarantor === false ?
+                        <button type="button" className="bg-blue-800 hover:bg-blue-700 text-white font-bold font-sans p-4 text-lg md:text-xl" onClick={() => setWantGuarantor(true)}>Add Guarantor</button>
+                    : <Guarantor />
+                }
+            </div>
 
             <span
               className="font-bold font-sans text-center text-sm md:text-md text-red-500">
